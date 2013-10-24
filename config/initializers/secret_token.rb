f13f0089b484f9b8c08df665e96a85bd7833648e
Austin Stanhope <Austin_Stanhope@Intuit.com>
2013-10-24 13:38:32 -0700

    Initial commitasdfafafafdsafd.slice!(1..3)

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DemoApp::Application.config.secret_token = '9c2f0cba0ea5e5ecc1c1265f7d83eb2e35bc77d71d13574242394afb675aa7d6982b1ccc8aa2c4ba287cfd108254ca3bbdc5c475f1cfce8ead74ffde5430f3dd'
